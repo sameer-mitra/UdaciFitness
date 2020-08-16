f571e3a218283c64e29f53879078ba115f786140
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-08-17 07:52:16 +1000

    SM 20200817 Added pizza icon

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from 'expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <Ionicons name='ios-pizza' color='red' size={100} />
       <Text>Opening up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
