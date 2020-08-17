f9828b0c88b5ac3455898f012bb6b2a84c69b04e
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-08-17 18:12:39 +1000

    SM 20200817 Added AddEntry.js

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    <Ionicons name='ios-pizza' color='red' size={100} />
-      <Text>Opening up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
