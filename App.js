87498da706792ae00bc503dd5861101233b515e1
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-08-18 20:07:15 +1000

    SM 20200818 Added more details to Slider and Stepper

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
 
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <View>
         <AddEntry />
